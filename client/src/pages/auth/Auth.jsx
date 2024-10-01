aeef2698b53b77b8d51b036e203e4dd5a1c0a030
Lucas Menon <lucasguilarduccimenon@gmail.com>
2024-10-01 15:18:42 -0300

    Starting the new project

---
+++ client/src/pages/auth/Auth.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Auth = () => {
+  return (
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+      <h1>Auth</h1> 
+    </div>
+  );
+};
+
+export default Auth;
