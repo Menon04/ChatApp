b659d5affbda4dfff93682578a376b663445fb48
Lucas Menon <lucasguilarduccimenon@gmail.com>
2024-10-01 18:27:46 -0300

    Starting to build auth.jsx

--- client/src/pages/auth/Auth.jsx
+++ client/src/pages/auth/Auth.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Background from "../../assets/login2.png";
+import Peace from "../../assets/peace.svg";
 
 const Auth = () => {
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
-      <h1>Auth</h1> 
+      <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70] xl:w-[60vw] rounded-3xl grid xl:group-cols-2">
+        <div className="flex flex-col gap-10 items-center justify-center">
+          <div className="flex items-center justify-center flex-col">
+            <div className="flex items-center justify-center">
+              <h1 className="text-5xl font-bold md:text-6xl">Auth</h1>
+              <img src={Peace} alt="peace emoji" className="h-[100px]"></img>
+            </div>
+            <p className="font-medium text-center">Cheque os detalhes para poder começar a usar o melhor app de texto!!</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
